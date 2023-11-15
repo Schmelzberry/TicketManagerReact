cb812b6aef110dca7683dac26917407b99fc2597
Jake Elsberry <jelsberry96@gmail.com>
2023-11-15 11:08:56 -0800

    css style objects added

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Header from "./Header";
 import TicketControl from "./TicketControl";
+import MyStyledComponent from "./myStyledComponent";
 
 function App(){
   return (
     <React.Fragment>
       <Header />
       <TicketControl />
+      <MyStyledComponent />
     </React.Fragment>
   );
 }
